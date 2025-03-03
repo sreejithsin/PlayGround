dc1073bf0bfd0f322b5d976c3fdf6bef81c7271e
sreejithsin <sreejith.s@gmail.com>
2025-03-03 12:15:06 +0530

    Code refined, in a loop

--- crawl4ai/main.py
+++ crawl4ai/main.py
@@ -24,7 +24,6 @@ async def main():
         verbose=True,
     )
 
-    # Crawler configuration
     crawler_config = CrawlerRunConfig(
         cache_mode=CacheMode.BYPASS,
         extraction_strategy=JsonCssExtractionStrategy(schema),
@@ -33,44 +32,38 @@ async def main():
 
     # Run the crawler
     async with AsyncWebCrawler(config=browser_config) as crawler: 
-        result = await crawler.arun(
-            url="https://www.amazon.com/s?k=mobile+phone&crid=370OJ37JU1BF1&sprefix=mobile+pho%2Caps%2C353&ref=nb_sb_noss_2",
-            config=crawler_config
-        )
-        if result.success:
-            print ("Success")
-            products = json.loads(result.extracted_content)
-            print(products[:5])
-        else:
-            print("Error:", result.error_message)
 
         # Pagination JavaScript
         load_nextpage_js = ["document.getElementsByClassName('s-pagination-item s-pagination-next')[0].click();"]            
         # Wait for 
         wait_for_code = """() => document.getElementsByClassName('s-main-slot s-result-list s-search-results sg-row')[0].childElementCount > 10"""
 
-        # Page 2
-        next_page_conf = CrawlerRunConfig(
-            js_code=load_nextpage_js,
-            js_only=True,
-            extraction_strategy=JsonCssExtractionStrategy(schema),
-            session_id=session_name,
-            wait_for=wait_for_code,
-            cache_mode=CacheMode.BYPASS
-        )
+        # Crawl multiple pages
+        for page in range(3):
 
-        # Extract
-        result = await crawler.arun(
-            url="https://www.amazon.com/s?k=mobile+phone&crid=370OJ37JU1BF1&sprefix=mobile+pho%2Caps%2C353&ref=nb_sb_noss_2",
-            config=next_page_conf
-        )
+            # Crawler configuration
+            crawler_config = CrawlerRunConfig(
+                js_code=load_nextpage_js if page > 0 else None,
+                js_only=True if page > 0 else False,
+                extraction_strategy=JsonCssExtractionStrategy(schema),
+                session_id=session_name,
+                wait_for=wait_for_code if page > 0 else None,
+                cache_mode=CacheMode.BYPASS
+            )
 
-        if result.success:
-            print ("Success")
-            products = json.loads(result.extracted_content)
-            print(products[:5])
-        else:
-            print("Error:", result.error_message)        
+            # Crawl
+            result = await crawler.arun(
+                url="https://www.amazon.com/s?k=mobile+phone&crid=370OJ37JU1BF1&sprefix=mobile+pho%2Caps%2C353&ref=nb_sb_noss_2",
+                config=crawler_config
+            )
+
+            # Data
+            if result.success:
+                print (f"Success: Page {page+1}")
+                products = json.loads(result.extracted_content)
+                print(products[:5])
+            else:
+                print("Error:", result.error_message)   
 
 if __name__ == "__main__":
     asyncio.run(main())
